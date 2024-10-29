9b311188dafb799c3b701f6bdc7e49fb6237d079
NopGtb <npyyny19@student.oulu.fi>
2024-10-29 21:51:08 +0200

    SDMO: improved c3

--- metrics/external_tools/c3.py
+++ metrics/external_tools/c3.py
@@ -8,65 +8,63 @@ from pathlib import Path
 import numpy as np
 import json
 import javalang.tree
+import datetime
+from sklearn.decomposition import TruncatedSVD
+from sklearn.metrics.pairwise import cosine_similarity
+
 
 #Create co occurance matrix from the words. How many times did each word occur with its pair in the context?
-def create_co_occurance_matrix(m_words, all_words):
+def create_co_occurance_matrix(m_words, word_map, unique_word_count):
     #Each array (variable/comment) in the words is treated as its own context window
-    single_dim_uniq_words = list(set([w for c in all_words for w in c]))
-    word_mat_map = {w:i for i,w in enumerate(single_dim_uniq_words)}
-    word_count = len(single_dim_uniq_words)
-    #word_count * word_count zero matrix
-    co_occurance = np.zeros((word_count, word_count), dtype=np.int32)
+    #unique_word_count * unique_word_count zero matrix
+    co_occurance = np.zeros((unique_word_count, unique_word_count), dtype=np.int32)
     #Loop trough each context
     for context in m_words:
         #Loop trough each word in context
-        for word_1 in context:
-        #Loop trough the same context again and set values in 
-            for word_2 in context:
-                #Ignore word itself
-                if not word_1 == word_2:
-                    row = word_mat_map[word_1]
-                    column = word_mat_map[word_2]
-                    co_occurance[row, column] = co_occurance[row, column] + 1
-    #word_count * word_count co_occurance matrix
+        word_counts = {}
+        #Findout how many times did each word occur
+        for word in context:
+            word_counts[word] = word_counts.get(word, 0) + 1
+        
+        word_1 = 0
+        word_2 = 0
+        context_unique_words = list(word_counts.keys())
+        #Build co occurance matrix
+        while word_1 < len(context_unique_words) -1:
+            word_2 = (word_1 + 1)
+            while word_2 < len(context_unique_words):
+                #Find out words place in matrix and their co occurance value
+                word_1_index = word_map[context_unique_words[word_1]]
+                word_2_index = word_map[context_unique_words[word_2]]
+                word_co_occurance_value = min(word_counts[context_unique_words[word_1]], word_counts[context_unique_words[word_2]])
+                #Set value for both letters
+                co_occurance[word_1_index, word_2_index] = word_co_occurance_value
+                co_occurance[word_2_index, word_1_index] = word_co_occurance_value
+                word_2 = word_2 + 1
+            word_1 = word_1 + 1
     return co_occurance
 
-#Calculates svd and reduction for the given matrix
-def svd(co_occurance_matrix):
-    k = 3
-    #Calculate svd
-    U, S, Vh = np.linalg.svd(co_occurance_matrix)
-    #Turn s into a diag matrix
-    s_diag = np.zeros(co_occurance_matrix.shape)
-    np.fill_diagonal(s_diag, S)
-    #Reduce the svd output
-    u_k = U[:, :k],
-    s_k = s_diag[:k, :k]
-    vh_k = Vh[:k, :]
-    #Reconstruct reduced matrix
-    #Approximates the orignal while capturing significant features in reduced state
-    return np.dot(u_k, np.dot(s_k, vh_k))
+#Calculates truncated svd
+def svd(co_occurance_matrix, truncsvd):
+    return truncsvd.fit_transform(co_occurance_matrix)
 
 #Calculates the cosine similarity of the two given lsi values
 def cms(lsi1, lsi2):
-    #Flatten to vector
-    lsi1 = lsi1.flatten()
-    lsi2 = lsi2.flatten()
-    #Calculate norms
-    lsi1_norm = np.linalg.norm(lsi1)
-    lsi2_norm = np.linalg.norm(lsi2)
-    #Avoid div by 0
-    if not lsi1_norm == 0 and not lsi2_norm == 0:
-        return np.dot(lsi1, lsi2) / (lsi1_norm * lsi2_norm)
-    return 0.0
+    cs = cosine_similarity(lsi1.reshape(1,-1), lsi2.reshape(1,-1))
+    if cs[0,0] > 0:
+        return cs[0,0]
+    return 0
 
 #Calculates the LSI value for the given words
-def lsi(m_words, all_words):
+def lsi(m_words, word_map, unique_word_count, truncsvd):
     #Create co-occurance matrix from the given words
-    co_occurance_matrix = create_co_occurance_matrix(m_words, all_words)
+    co_occurance_matrix = create_co_occurance_matrix(m_words, word_map, unique_word_count)
     #Reduce using svd
-    svd_reduced = svd(co_occurance_matrix)
-    return svd_reduced
+    if np.max(co_occurance_matrix) > 0:
+        #If max is 0 svd will error
+        svd_reduced = svd(co_occurance_matrix, truncsvd)
+        return svd_reduced
+    return None
 
 #Splits CamelCase and vars_seperated_using a _ into individual words
 def split_name(name):
@@ -171,19 +169,33 @@ def acsm(c, code):
         all_words.extend(m["words"])
     #Pre calculate LSI value for each method in class
     cms_values = []
-    for m in methods:
-        m["lsi_value"] = lsi(m["words"], all_words)
+    min_dimensions = 3
+    #We need atleast 3 words total
+    if len(all_words) >= min_dimensions:
+        #Calculate word matrix dimensions and a word => mat map
+        single_dim_uniq_words = list(set([w for c in all_words for w in c]))
+        word_map = {w:i for i,w in enumerate(single_dim_uniq_words)}
+        word_count = len(single_dim_uniq_words)
+        truncsvd = TruncatedSVD(n_components=min_dimensions)
+        for m in methods:
+            m["lsi_value"] = lsi(m["words"], word_map, word_count, truncsvd)
 
-    #Calculate cosine similarity for each method LSI pair
-    for i, lsi1 in enumerate(methods):
-        for j, lsi2 in enumerate(methods):
-            if not i == j:
-                #non diagonial pairs only
-                cms_values.append(cms(lsi1["lsi_value"], lsi2["lsi_value"]))
-    #If we have cms values
-    if cms_values:
-        #Return the average cms value
-        return sum(cms_values) / len(cms_values)
+        method_1 = 0
+        method_2 = 0
+        #Roll trough LSI pairs once calculating cms
+        #Dot(a,b) == Dot(b,a) 
+        #Average wont be affected by missing extra pair of same values
+        while method_1 < len(methods) -1:
+            method_2 = (method_1 + 1)
+            while method_2 < len(methods):
+                if isinstance(methods[method_1]["lsi_value"], np.ndarray) and isinstance(methods[method_2]["lsi_value"], np.ndarray):
+                    cms_values.append(cms(methods[method_1]["lsi_value"], methods[method_2]["lsi_value"]))
+                method_2 = method_2 + 1
+            method_1 = method_1 + 1
+        #If we have cms values
+        if cms_values:
+            #Return the average cms value
+            return sum(cms_values) / len(cms_values)
     return 0
 
 #Calculates the c3 value for the given class
@@ -245,6 +257,10 @@ def write_json(path, data):
 def folder_exists(path):
     return os.path.exists(path)
 
+#Gets a formated timestamp
+def get_timestamp():
+    return datetime.datetime.now().strftime('%H:%M:%S')
+
 #Get console arguments
 parser = argparse.ArgumentParser("Given path containing java files, calculates c3 value for the classes contained in them")
 parser.add_argument("target", help="Absolute path containing targeted files", type=str)
@@ -254,12 +270,14 @@ args = parser.parse_args()
 target_path = args.target
 if folder_exists(target_path):
     java_files = get_java_files_in_path(target_path)
+    print(get_timestamp(), ": parsing files")
     parsed_java = parse_java_files(java_files)
-
+    print(get_timestamp(), ": parsed files")
     result_per_class = []
     #For each file
     for unit in parsed_java:
         #Get classes
+        print(get_timestamp(), ": working on ", unit["path"])
         unit_classes = get_file_classes(unit["parse"])
         for c in unit_classes:
             #For each class calculate C3 metric
